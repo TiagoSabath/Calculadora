5263432c9f5ae3bf156f403e1d23375db08d8ede
TiagoSabath <tiago.sabath@gmail.com>
2023-10-30 10:25:56 -0300

    first commit

---
+++ Models/Calculo.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculadora.Models
+{
+    public class Calculo
+    {
+        public void Somar(int x, int y)
+        {
+            Console.WriteLine($"{x} + {y} = {x + y}");
+        }
+
+        public void Subtrair(int x, int y)
+        {
+            Console.WriteLine($"{x} - {y} = {x - y}");
+        }
+
+        public void Multiplicar(int x, int y)
+        {
+            Console.WriteLine($"{x} X {y} = {x * y}");
+        }
+
+        public void Dividir(int x, int y)
+        {
+            Console.WriteLine($"{x} / {y} = {x / y}");
+        }
+
+        public void Pontecia(int x, int y)
+        {
+            double pot = Math.Pow(x, y);
+            Console.WriteLine($"{x} ^ {y} = {pot}");
+        }
+
+        public void Seno(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double seno = Math.Sin(radiano);
+            Console.WriteLine($"Seno de {angulo} = {Math.Round(seno,4)}");  //Math.Round(seno,4) mostra 4 números
+        }
+
+        public void Coseno(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double coseno = Math.Cos(radiano);
+            Console.WriteLine($"Coseno de {angulo} = {Math.Round(coseno,4)}");
+        }
+
+        public void Tangente(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double tangente = Math.Tan(radiano);
+            Console.WriteLine($"Tangente de {angulo} = {Math.Round(tangente,4)}");
+        }
+
+        public void RaizQuadrada(double x){
+            double raiz = Math.Sqrt(x);
+            Console.WriteLine($"Raiz Quadrada dde {x} = {raiz}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿using Calculadora.Models;
+
+Calculo calc = new Calculo();
+
+calc.Somar(10,15);
+calc.Subtrair(5,3);
+calc.Multiplicar(5,3);
+calc.Dividir(10,2);
+calc.Pontecia(3,3);
+calc.Seno(30);
+calc.Coseno(30);
+calc.Tangente(30);
+calc.RaizQuadrada(9); 
+
+
+// ------- Laço de repetição For Tabuada ----------
+
+int numero = 10;
+
+for (int contador = 0; contador <= 10; contador++)
+{
+    Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+} 
+
+
+// ------- Laço de repetição While Tabuada ----------
+
+int numero = 10;
+int contador = 0;
+
+while(contador <= 10)
+{
+    Console.WriteLine($"{contador} Execução: {numero} x {contador} = {numero * contador}");
+    contador++;
+} 
+
+
+// ------- Laço de repetição Do While Tabuada ----------
+
+int soma = 0, numero = 0;
+
+
+do
+{
+    Console.WriteLine("Digite um número: (0 para parar)");
+    numero = Convert.ToInt32(Console.ReadLine());
+
+    soma += numero; 
+
+} while(numero != 0);
+
+    Console.WriteLine($"Total da soma dos números é: {soma}");
+
+
+    // ------- Menu  ----------
+
+    string opcao;
+    bool exebirMenu = true;
+
+    while (exebirMenu)
+    {
+        Console.Clear();
+        Console.WriteLine("Digite sua opção: ");
+        Console.WriteLine("1 - Cadastrar um cliente");
+        Console.WriteLine("2 - Buscar um cliente");
+        Console.WriteLine("3 - Apagar um cliente");
+        Console.WriteLine("4 - Encerra");
+
+        opcao = Console.ReadLine();
+
+        switch (opcao)
+        {
+            case "1":
+                Console.WriteLine("Cadastro de cliente");
+                break;
+
+            case "2":
+                Console.WriteLine("Buscar de cliente");
+                break;
+
+            case "3":
+                Console.WriteLine("Apagar um cliente");
+                break;
+
+            case "4":
+                Console.WriteLine("Encerra");
+                exebirMenu = false;
+                //Environment.Exit(0);   encerra o menu e nao execultar mais nada
+                break;  
+
+
+            default:
+                Console.WriteLine("Opção invalida");
+                break;
+        }
+    }
+
+    Console.WriteLine("O programa se encerrou");
